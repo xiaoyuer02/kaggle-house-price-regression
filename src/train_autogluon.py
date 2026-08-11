9f6d0bad1b5a4991e39577afb0141fee7baaffaa
xiaoyuer02 <1661822947@qq.com>
2026-08-11 19:19:26 +0800

    Update

---
+++ src/train_autogluon.py
@@ -0,0 +1,30 @@
+from autogluon.tabular import TabularPredictor, TabularDataset
+import numpy as np
+
+# load data
+train_data = TabularDataset('data/train.csv')
+
+# preprocessing
+col, label = 'Id', 'SalePrice'
+labels = ['LotArea', 'TotalBsmtSF', '1stFlrSF', 'GrLivArea']
+for label in labels + ['SalePrice']:
+    train_data[label] = np.log1p(train_data[label])
+
+# train   
+predictor = TabularPredictor(label = label).fit(train_data.drop(columns = [col]))
+
+# predict
+import pandas as pd
+test_data = TabularDataset('data/test.csv')
+
+for label in labels:
+    test_data[label] = np.log1p(test_data[label])
+
+# predict
+preds = predictor.predict(test_data.drop(columns = [col]))
+
+# submit
+submission = pd.DataFrame({
+    'Id': test_data[col], 
+    'SalePrice': np.expm1(preds)})
+submission.to_csv('submission_autogluon.csv', index = False)
